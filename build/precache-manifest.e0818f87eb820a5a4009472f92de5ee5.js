543df6720ea6a34d3198ec1548af3f1ba5d29091
duniansampa <duniansampa@outlook.com>
2019-08-26 00:27:23 -0400

    -add build folder

---
+++ build/precache-manifest.e0818f87eb820a5a4009472f92de5ee5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "271c7078b6b3ec9eb200",
+    "url": "./static/css/main.4f709573.chunk.css"
+  },
+  {
+    "revision": "271c7078b6b3ec9eb200",
+    "url": "./static/js/main.271c7078.chunk.js"
+  },
+  {
+    "revision": "a7f863ba070b5a6434b1",
+    "url": "./static/js/1.a7f863ba.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "5987dd12fea78ce5f97ae601b08ec03c",
+    "url": "./static/media/nucleo.5987dd12.woff2"
+  },
+  {
+    "revision": "f0b489a5dbbff08833d21024f9fcbd4e",
+    "url": "./static/media/nucleo.f0b489a5.woff"
+  },
+  {
+    "revision": "03ef1918e505c3e3471f9369ef7a638f",
+    "url": "./static/media/nucleo.03ef1918.eot"
+  },
+  {
+    "revision": "b17a118e13e53558658b681a0ebdad82",
+    "url": "./static/media/nucleo.b17a118e.ttf"
+  },
+  {
+    "revision": "ac9b978b029e0bf424e942314a77eeb6",
+    "url": "./index.html"
+  }
+];
